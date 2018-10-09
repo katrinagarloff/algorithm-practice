d2a9e5b5f9ca67900287f753d513331deb800be2
katrinagarloff <katrinagarloff@gmail.com>
2018-10-09 16:12:47 -0400

    initial anagram solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,32 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  const aSplit = stringA.replace(/[^\w]/g, "").toLowerCase().split("");
+  const bSplit = stringB.replace(/[^\w]/g, "").toLowerCase().split("");
+  const aHash = {};
+  const bHash = {};
+  let isAnagram = true;
+
+ for (char of aSplit) {
+   aHash[char] ? aHash[char] += 1 : aHash[char] = 1;
+ }
+
+ for (char of bSplit) {
+   bHash[char] ? bHash[char] += 1 : bHash[char] = 1;
+
+ }
+
+if (Object.keys(aHash).length !== Object.keys(bHash).length) {
+  return false;
+} else {
+  for (char in aHash) {
+    if (aHash[char] !== bHash[char]) {
+      isAnagram = false;
+    }
+  }
+}
+return isAnagram;
+}
 
 module.exports = anagrams;
