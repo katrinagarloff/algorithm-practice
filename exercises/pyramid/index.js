356f5c8eeebc20a65313d49f22b784cdb74f9a73
katrinagarloff <katrinagarloff@gmail.com>
2018-10-12 16:08:32 -0400

    initial solution pyramids

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,42 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+
+function pyramid(n, row = 0, column = 0, level="") {
+  const numberOfColumns = (n * 2) -2
+
+  if (row === n) {
+    return;
+  }
+
+  if(column > numberOfColumns) {
+    console.log(level)
+    return pyramid(n, row + 1);
+  }
+
+  if (column >= Math.floor(numberOfColumns/2 - row) && column <= Math.floor(numberOfColumns/2 + row)) {
+    level += "#";
+  } else {
+    level += " "
+  }
+
+  return pyramid(n, row, column + 1, level);
+}
+// function pyramid(n) {
+// const numberOfColumns = (n * 2) - 2;
+// let level = "";
+//
+// for (let row = 0; row < n; row++) {
+//   for (let column = 0; column <= numberOfColumns; column++) {
+//     if (column >= Math.floor(numberOfColumns/2 - row) && column <= Math.floor(numberOfColumns/2 + row)) {
+//       level += "#";
+//     } else {
+//       level += " ";
+//     }
+//    }
+//    console.log(level);
+//    level = "";
+//   }
+// }
 
 module.exports = pyramid;
